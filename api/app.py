457741f4c4a14da3f451cd75c4fd443ad3bda047
John Gunderson <john.gunderson@yale.edu>
2023-06-23 10:33:50 -0400

    Flask API and basic camera frontend

--- api/app.py
+++ api/app.py
@@ -1,49 +1,79 @@
-from flask import *
-from eye_tracking import track
-import os 
+import base64
+import os
+import eye_tracking
 import cv2
+import numpy as np
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO, emit
 
-UPLOAD_FOLDER = 'C:\\Users\\johng\\Desktop\\'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app = Flask(__name__, static_folder="./templates/static")
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app, async_mode="eventlet")
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route("/upload/", methods=["POST", "GET"])
-def upload():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            print('fail')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print('saved file')
-            img = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            res = track(img, 120)
-            return jsonify(res)
-    return ''
-
-# @app.route('/get_direction/<filename>')
-# def get_direction(filename):
-#     img = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#     res = track(img)
-#     return jsonify(res)
+def base64_to_image(base64_string):
+    """
+    The base64_to_image function accepts a base64 encoded string and returns an image.
+    The function extracts the base64 binary data from the input string, decodes it, converts 
+    the bytes to numpy array, and then decodes the numpy array as an image using OpenCV.
+    
+    :param base64_string: Pass the base64 encoded image string to the function
+    :return: An image
+    """
+    base64_data = base64_string.split(",")[1]
+    image_bytes = base64.b64decode(base64_data)
+    image_array = np.frombuffer(image_bytes, dtype=np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    return image
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+@socketio.on("connect")
+def test_connect():
+    """
+    The test_connect function is used to test the connection between the client and server.
+    It sends a message to the client letting it know that it has successfully connected.
+    
+    :return: A 'connected' string
+    """
+    print("Connected")
+    emit("my response", {"data": "Connected"})
+
+
+
+
+@socketio.on("image")
+def receive_image(image):
+    """
+    The receive_image function takes in an image from the webcam, converts it to grayscale, and then emits
+    the processed image back to the client.
+
+
+    :param image: Pass the image data to the receive_image function
+    :return: The image that was received from the client
+    """
+    # Decode the base64-encoded image data
+    image = base64_to_image(image)
+    frame_resized = cv2.resize(image, (640, 360))
+
+    eye = eye_tracking.track(frame_resized, 120)
+    if eye is None:
+        return
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    result, frame_encoded = cv2.imencode(".jpg", eye, encode_param)
+    processed_img_data = base64.b64encode(frame_encoded).decode()
+    b64_src = "data:image/jpg;base64,"
+    processed_img_data = b64_src + processed_img_data
+    emit("processed_image", processed_img_data)
+
+
+@app.route("/")
+def index():
+    """
+    The index function returns the index.html template, which is a landing page for users.
+    
+    :return: The index
+    """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True, port=5000, host='0.0.0.0')
--- api/eye_tracking.py
+++ api/eye_tracking.py
@@ -5,10 +5,15 @@ import numpy as np
 # init part
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-detector_params = cv2.SimpleBlobDetector_Params()
-detector_params.filterByArea = True
-detector_params.maxArea = 1500
-detector = cv2.SimpleBlobDetector_create(detector_params)
+params = cv2.SimpleBlobDetector_Params()
+
+params.filterByArea = True
+params.maxArea = 1500
+params.filterByCircularity = True
+params.minCircularity = 0.5
+
+# Set up the detector with default parameters.
+detector = cv2.SimpleBlobDetector_create(params)
 
 
 def detect_faces(img, cascade):
@@ -57,12 +62,11 @@ def cut_eyebrows(img):
 
 def blob_process(img, threshold, detector):
     gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
-    img = cv2.erode(img, None, iterations=2)
-    img = cv2.dilate(img, None, iterations=4)
-    img = cv2.medianBlur(img, 5)
-    keypoints = detector.detect(img)
-    print(keypoints)
+    # _, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
+    # img = cv2.erode(img, None, iterations=2)
+    # img = cv2.dilate(img, None, iterations=4)
+    # img = cv2.medianBlur(img, 5)
+    keypoints = detector.detect(gray_frame)
     return keypoints
 
 
@@ -71,8 +75,14 @@ def nothing(x):
 
 
 def track(img, threshold):
+    ###################################################
+
+    # FIX THIS SECTION
+    # img is the input image of the face. This function should return the image that is output on the screen
+
+    ###################################################
     face_frame = detect_faces(img, face_cascade)
-    fractions = []
+    eye = None
     if face_frame is not None:
         eyes = detect_eyes(face_frame, eye_cascade)
         for eye in eyes:
@@ -80,11 +90,11 @@ def track(img, threshold):
                 eye = cut_eyebrows(eye)
                 keypoints = blob_process(eye, threshold, detector)
                 eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-                # return fraction of the eye image that keypoint is in
-                w = len(eye)
-                fraction = keypoints[0].pt[0]/w
-                fractions.append(fraction)
+                # # return fraction of the eye image that keypoint is in
+                # w = len(eye)
+                # fraction = keypoints[0].pt[0]/w
+                # fractions.append(fraction)
     # return 0 if eye is left, 1 otherwise
-    return fractions
+    return eye
 
 
